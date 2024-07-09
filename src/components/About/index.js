0e774ee346c1eb910f76c76660cd3b3a0e1087e6
Joel Azeta <ajfizzle310@outlook.com>
2024-07-09 18:24:35 -0500

    Learn to Code - updated App.css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,34 +5,32 @@ function About() {
     <section id="about-me">
       <div className="section-container">
         <h2 className="section-title">About Me</h2>
-      </div>
-      <div className="section-border">
-        <article>
-          <div className="about-text">
-            <p>
-              I'm a passionate problem-solver with a diverse background in the
-              world of technology. My journey began as a traditional deskside
-              support which I transitioned to Application Support, where I honed
-              my skills in troubleshooting, diagnosing, and resolving complex
-              technical issues. I thrived in fast-paced environments, providing
-              exceptional support to users of all technical levels.
-            </p>
-            <br />
-            <p>
-              I constantly seek to learn and explore new technologies, staying
-              abreast of the latest trends and advancements. I believe in the
-              power of teamwork and leverage diverse perspectives to achieve
-              optimal solutions.
-            </p>
-            <br />
-            <p>
-              While my professional life revolves around technology, I believe
-              in maintaining a healthy balance by enjoying activities such as
-              cooking, watching soccer, football, hiking, biking and movies, and
-              spending time with my spouse and our wonderful kids.
-            </p>
-          </div>
-        </article>
+        <div className="section-border">
+          <article>
+            <div className="about-text">
+              <p>
+                I'm a passionate problem-solver with a diverse background in the
+                world of technology. My journey began as a traditional deskside
+                support which I transitioned to Application Support, where I
+                honed my skills in troubleshooting, diagnosing, and resolving
+                complex technical issues. I thrived in fast-paced environments,
+                providing exceptional support to users of all technical levels.
+              </p>
+              <p>
+                I constantly seek to learn and explore new technologies, staying
+                abreast of the latest trends and advancements. I believe in the
+                power of teamwork and leverage diverse perspectives to achieve
+                optimal solutions.
+              </p>
+              <p>
+                While my professional life revolves around technology, I believe
+                in maintaining a healthy balance by enjoying activities such as
+                cooking, watching soccer, football, hiking, biking and movies,
+                and spending time with my spouse and our wonderful kids.
+              </p>
+            </div>
+          </article>
+        </div>
       </div>
     </section>
   );
