c6890bd991439ac394ba7eb324ac9af70655c086
Joel Azeta <ajfizzle310@outlook.com>
2024-07-10 18:17:13 -0500

    Learn to code - updated App.css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer id="bg-footer" className="footer-section">
-      <h1>Made with ❤️️ UTA Coding Bootcamp©</h1>
+      <h1>Made with ❤️️ UTA Coding Bootcamp ©2024</h1>
     </footer>
   );
 }
