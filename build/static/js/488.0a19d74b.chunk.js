9248c4f3cb880c5c2fd6a07fdedec3937b2d2592
Joel Azeta <ajfizzle310@outlook.com>
2026-09-06 05:05:24 -0500

    "Fix react-scripts dependency"

---
+++ build/static/js/488.0a19d74b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkreact_portfolio_challenge||=[]).push([[488],{488(){new Set,new WeakMap}}]);
+//# sourceMappingURL=488.0a19d74b.chunk.js.map
--- build/static/js/488.2f22abdc.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(globalThis.webpackChunkreact_portfolio_challenge||=[]).push([[488],{488(){new Map,new Date}}]);
-//# sourceMappingURL=488.2f22abdc.chunk.js.map
